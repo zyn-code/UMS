f57a6c166a823f51ef2116997961c5a9dc383ea9
PC <201910310@ua.edu.lb>
2022-07-12 06:32:17 -0700

    Add OData Configuration

--- UMS.Application/Entities/Course/Commands/InsertCourse/InsertCourseHandler.cs
+++ UMS.Application/Entities/Course/Commands/InsertCourse/InsertCourseHandler.cs
@@ -7,7 +7,7 @@ public class InsertCourseHandler : MediatR.IRequestHandler<InsertCourseCommand, 
 {
     private readonly umsContext _context = new umsContext();
     public async Task<string> Handle(InsertCourseCommand request, CancellationToken cancellationToken)
-    {
+    {   
         //request.course.EnrolmentDateRange = _context.Courses.First().EnrolmentDateRange;
         var res = _context.Courses.AddAsync(request.course);
         Console.WriteLine("Result : " + res);
