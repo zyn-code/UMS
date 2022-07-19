cda9cdbc02dc379d576b1d1b0cef8bbeca96d764
PC <201910310@ua.edu.lb>
2022-07-19 14:14:21 -0700

    First Commit

--- UMS.Infrastructure.Abstraction/EmailSenderInterface/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-using EmailServiceTools;
-using MimeKit;
-using NETCore.MailKit;
-namespace UMS.Infrastructure.Abstraction.EmailSenderInterface;
-
-public interface IEmailSender
-{
-    Task SendEmailAsync(Message message);
-}
