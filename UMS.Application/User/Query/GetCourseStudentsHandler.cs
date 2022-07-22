b16ff6f9a66bcf8753939922c5dd23b990c40c74
Ziad <ziadyoutanji123@gmail.com>
2022-07-22 10:32:22 +0300

    Add Get Students By Course Id, Fix Bug : Course Add Course Not Working

---
+++ UMS.Application/User/Query/GetCourseStudentsHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using UMS.Persistence;
+
+namespace UMS.Application.User.Query;
+
+public class GetCourseStudentsHandler : IRequestHandler<GetCourseStudentsQuery,List<Domain.Models.User>>
+{
+    private readonly umsContext _context;
+
+    public GetCourseStudentsHandler(umsContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<Domain.Models.User>> Handle(GetCourseStudentsQuery request, CancellationToken cancellationToken)
+    {
+        return _context.ClassEnrollments.Where(u => u.Class.Course.Name == request.CourseName)
+            .Select(u=>u.Student).ToList();
+    }
+}
